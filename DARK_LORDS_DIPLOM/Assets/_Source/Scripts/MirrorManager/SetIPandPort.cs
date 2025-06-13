5c9c6fa903bbd44b9ebccf68e0e3fa0383102b30
CHAINSAWXERI <123538769+CHAINSAWXERI@users.noreply.github.com>
2025-06-13 22:01:46 +0300

    fixfix

---
+++ DARK_LORDS_DIPLOM/Assets/_Source/Scripts/MirrorManager/SetIPandPort.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using Mirror;
+using kcp2k;
+
+public class SetIPandPort : MonoBehaviour
+{
+    [SerializeField] public NetworkManager NetworkManager;
+    [SerializeField] public KcpTransport KcpTransport;
+    [SerializeField] public TMP_InputField ipInputField;
+    [SerializeField] public TMP_InputField portInputField;
+
+    public void SetIPAndPort()
+    {
+        if (ipInputField != null)
+        {
+            NetworkManager.networkAddress = ipInputField.text;
+            if (ushort.TryParse(portInputField.text, out ushort port))
+            {
+                KcpTransport.Port = port;
+            }
+            else
+            {
+                Debug.LogError("Некорректный порт! Введите число от 0 до 65535");
+            }
+        }
+        else
+        {
+            Debug.LogError("TMP_InputField не назначен!");
+        }
+    }
+}

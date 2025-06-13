8ef5e48f933fc627a652f733fb4c1705c833d628
CHAINSAWXERI <123538769+CHAINSAWXERI@users.noreply.github.com>
2025-06-13 23:21:53 +0300

    Update HealByYourselfAbility.cs

--- DARK_LORDS_DIPLOM/Assets/_Source/Scripts/PassiveAbilitiesCardSystems/HealByYourselfAbility.cs
+++ DARK_LORDS_DIPLOM/Assets/_Source/Scripts/PassiveAbilitiesCardSystems/HealByYourselfAbility.cs
@@ -38,7 +38,7 @@ public class HealByYourselfAbility : PassiveAbility
                         fieldOn.SelfCard.Health = fieldOn.SelfCard.Health - diff;
                         gameManager.CardPlayerField1.ShowCardInfo(gameManager.CardPlayerField1.SelfCard, gameManager.CardPlayerField1.ID, gameManager, gameManager.CardPlayerField1.WhoseCard);
                         fieldOn.ShowCardInfo(fieldOn.SelfCard, fieldOn.ID, gameManager, fieldOn.WhoseCard);
-                        //return;
+                        return;
                     }
                     else
                     {
@@ -75,7 +75,7 @@ public class HealByYourselfAbility : PassiveAbility
                         fieldOn.SelfCard.Health = fieldOn.SelfCard.Health - diff;
                         gameManager.CardPlayerField2.ShowCardInfo(gameManager.CardPlayerField2.SelfCard, gameManager.CardPlayerField2.ID, gameManager, gameManager.CardPlayerField2.WhoseCard);
                         fieldOn.ShowCardInfo(fieldOn.SelfCard, fieldOn.ID, gameManager, fieldOn.WhoseCard);
-                        //return;
+                        return;
                     }
                     else
                     {
@@ -112,7 +112,7 @@ public class HealByYourselfAbility : PassiveAbility
                         fieldOn.SelfCard.Health = fieldOn.SelfCard.Health - diff;
                         gameManager.CardPlayerField3.ShowCardInfo(gameManager.CardPlayerField3.SelfCard, gameManager.CardPlayerField3.ID, gameManager, gameManager.CardPlayerField3.WhoseCard);
                         fieldOn.ShowCardInfo(fieldOn.SelfCard, fieldOn.ID, gameManager, fieldOn.WhoseCard);
-                        //return;
+                        return;
                     }
                     else
                     {
@@ -149,7 +149,7 @@ public class HealByYourselfAbility : PassiveAbility
                         fieldOn.SelfCard.Health = fieldOn.SelfCard.Health - diff;
                         gameManager.CardPlayerField4.ShowCardInfo(gameManager.CardPlayerField4.SelfCard, gameManager.CardPlayerField4.ID, gameManager, gameManager.CardPlayerField4.WhoseCard);
                         fieldOn.ShowCardInfo(fieldOn.SelfCard, fieldOn.ID, gameManager, fieldOn.WhoseCard);
-                        //return;
+                        return;
                     }
                     else
                     {
@@ -179,7 +179,7 @@ public class HealByYourselfAbility : PassiveAbility
                         gameManager.CardEnemyField1.SelfCard.Health = gameManager.CardEnemyField1.SelfCard.Health + fieldOn.SelfCard.Health;
                         fieldOn.SelfCard.Health = fieldOn.SelfCard.Health - diff;
                         gameManager.CardEnemyField1.ShowCardInfo(gameManager.CardEnemyField1.SelfCard, gameManager.CardEnemyField1.ID, gameManager, gameManager.CardEnemyField1.WhoseCard);
-                        //fieldOn.ShowCardInfo(fieldOn.SelfCard, fieldOn.ID, gameManager, fieldOn.WhoseCard);
+                        fieldOn.ShowCardInfo(fieldOn.SelfCard, fieldOn.ID, gameManager, fieldOn.WhoseCard);
                         //return;
                     }
                     if (vHelath == 0)
@@ -187,15 +187,15 @@ public class HealByYourselfAbility : PassiveAbility
                         gameManager.CardEnemyField1.SelfCard.Health = gameManager.CardEnemyField1.SelfCard.Health + diff;
                         fieldOn.SelfCard.Health = fieldOn.SelfCard.Health - diff;
                         gameManager.CardEnemyField1.ShowCardInfo(gameManager.CardEnemyField1.SelfCard, gameManager.CardEnemyField1.ID, gameManager, gameManager.CardEnemyField1.WhoseCard);
-                        //fieldOn.ShowCardInfo(fieldOn.SelfCard, fieldOn.ID, gameManager, fieldOn.WhoseCard);
-                        //return;
+                        fieldOn.ShowCardInfo(fieldOn.SelfCard, fieldOn.ID, gameManager, fieldOn.WhoseCard);
+                        return;
                     }
                     else
                     {
                         gameManager.CardEnemyField1.SelfCard.Health = gameManager.CardEnemyField1.SelfCard.Health + diff;
                         fieldOn.SelfCard.Health = fieldOn.SelfCard.Health - diff;
                         gameManager.CardEnemyField1.ShowCardInfo(gameManager.CardEnemyField1.SelfCard, gameManager.CardEnemyField1.ID, gameManager, gameManager.CardEnemyField1.WhoseCard);
-                        //fieldOn.ShowCardInfo(fieldOn.SelfCard, fieldOn.ID, gameManager, fieldOn.WhoseCard);
+                        fieldOn.ShowCardInfo(fieldOn.SelfCard, fieldOn.ID, gameManager, fieldOn.WhoseCard);
                     }
                 }
                 Debug.Log("V2 = " + fieldOn.SelfCard.Health);
@@ -215,7 +215,7 @@ public class HealByYourselfAbility : PassiveAbility
                         gameManager.CardEnemyField2.SelfCard.Health = gameManager.CardEnemyField2.SelfCard.Health + fieldOn.SelfCard.Health;
                         fieldOn.SelfCard.Health = fieldOn.SelfCard.Health - diff;
                         gameManager.CardEnemyField2.ShowCardInfo(gameManager.CardEnemyField2.SelfCard, gameManager.CardEnemyField2.ID, gameManager, gameManager.CardEnemyField2.WhoseCard);
-                        //fieldOn.ShowCardInfo(fieldOn.SelfCard, fieldOn.ID, gameManager, fieldOn.WhoseCard);
+                        fieldOn.ShowCardInfo(fieldOn.SelfCard, fieldOn.ID, gameManager, fieldOn.WhoseCard);
                         //return;
                     }
                     if (vHelath == 0)
@@ -223,15 +223,15 @@ public class HealByYourselfAbility : PassiveAbility
                         gameManager.CardEnemyField2.SelfCard.Health = gameManager.CardEnemyField2.SelfCard.Health + diff;
                         fieldOn.SelfCard.Health = fieldOn.SelfCard.Health - diff;
                         gameManager.CardEnemyField2.ShowCardInfo(gameManager.CardEnemyField2.SelfCard, gameManager.CardEnemyField2.ID, gameManager, gameManager.CardEnemyField2.WhoseCard);
-                        //fieldOn.ShowCardInfo(fieldOn.SelfCard, fieldOn.ID, gameManager, fieldOn.WhoseCard);
-                        //return;
+                        fieldOn.ShowCardInfo(fieldOn.SelfCard, fieldOn.ID, gameManager, fieldOn.WhoseCard);
+                        return;
                     }
                     else
                     {
                         gameManager.CardEnemyField2.SelfCard.Health = gameManager.CardEnemyField2.SelfCard.Health + diff;
                         fieldOn.SelfCard.Health = fieldOn.SelfCard.Health - diff;
                         gameManager.CardEnemyField2.ShowCardInfo(gameManager.CardEnemyField2.SelfCard, gameManager.CardEnemyField2.ID, gameManager, gameManager.CardEnemyField2.WhoseCard);
-                        //fieldOn.ShowCardInfo(fieldOn.SelfCard, fieldOn.ID, gameManager, fieldOn.WhoseCard);
+                        fieldOn.ShowCardInfo(fieldOn.SelfCard, fieldOn.ID, gameManager, fieldOn.WhoseCard);
                     }
                 }
                 Debug.Log("V2 = " + fieldOn.SelfCard.Health);
@@ -251,7 +251,7 @@ public class HealByYourselfAbility : PassiveAbility
                         gameManager.CardEnemyField3.SelfCard.Health = gameManager.CardEnemyField3.SelfCard.Health + fieldOn.SelfCard.Health;
                         fieldOn.SelfCard.Health = fieldOn.SelfCard.Health - diff;
                         gameManager.CardEnemyField3.ShowCardInfo(gameManager.CardEnemyField3.SelfCard, gameManager.CardEnemyField3.ID, gameManager, gameManager.CardEnemyField3.WhoseCard);
-                        //fieldOn.ShowCardInfo(fieldOn.SelfCard, fieldOn.ID, gameManager, fieldOn.WhoseCard);
+                        fieldOn.ShowCardInfo(fieldOn.SelfCard, fieldOn.ID, gameManager, fieldOn.WhoseCard);
                         //return;
                     }
                     if (vHelath == 0)
@@ -259,15 +259,15 @@ public class HealByYourselfAbility : PassiveAbility
                         gameManager.CardEnemyField3.SelfCard.Health = gameManager.CardEnemyField3.SelfCard.Health + diff;
                         fieldOn.SelfCard.Health = fieldOn.SelfCard.Health - diff;
                         gameManager.CardEnemyField3.ShowCardInfo(gameManager.CardEnemyField3.SelfCard, gameManager.CardEnemyField3.ID, gameManager, gameManager.CardEnemyField3.WhoseCard);
-                        //fieldOn.ShowCardInfo(fieldOn.SelfCard, fieldOn.ID, gameManager, fieldOn.WhoseCard);
-                        //return;
+                        fieldOn.ShowCardInfo(fieldOn.SelfCard, fieldOn.ID, gameManager, fieldOn.WhoseCard);
+                        return;
                     }
                     else
                     {
                         gameManager.CardEnemyField3.SelfCard.Health = gameManager.CardEnemyField3.SelfCard.Health + diff;
                         fieldOn.SelfCard.Health = fieldOn.SelfCard.Health - diff;
                         gameManager.CardEnemyField3.ShowCardInfo(gameManager.CardEnemyField3.SelfCard, gameManager.CardEnemyField3.ID, gameManager, gameManager.CardEnemyField3.WhoseCard);
-                        //fieldOn.ShowCardInfo(fieldOn.SelfCard, fieldOn.ID, gameManager, fieldOn.WhoseCard);
+                        fieldOn.ShowCardInfo(fieldOn.SelfCard, fieldOn.ID, gameManager, fieldOn.WhoseCard);
                     }
                 }
                 Debug.Log("V2 = " + fieldOn.SelfCard.Health);
@@ -287,7 +287,7 @@ public class HealByYourselfAbility : PassiveAbility
                         gameManager.CardEnemyField4.SelfCard.Health = gameManager.CardEnemyField4.SelfCard.Health + fieldOn.SelfCard.Health;
                         fieldOn.SelfCard.Health = fieldOn.SelfCard.Health - diff;
                         gameManager.CardEnemyField4.ShowCardInfo(gameManager.CardEnemyField4.SelfCard, gameManager.CardEnemyField4.ID, gameManager, gameManager.CardEnemyField4.WhoseCard);
-                        //fieldOn.ShowCardInfo(fieldOn.SelfCard, fieldOn.ID, gameManager, fieldOn.WhoseCard);
+                        fieldOn.ShowCardInfo(fieldOn.SelfCard, fieldOn.ID, gameManager, fieldOn.WhoseCard);
                         //return;
                     }
                     if (vHelath == 0)
@@ -295,8 +295,8 @@ public class HealByYourselfAbility : PassiveAbility
                         gameManager.CardEnemyField4.SelfCard.Health = gameManager.CardEnemyField4.SelfCard.Health + diff;
                         fieldOn.SelfCard.Health = fieldOn.SelfCard.Health - diff;
                         gameManager.CardEnemyField4.ShowCardInfo(gameManager.CardEnemyField4.SelfCard, gameManager.CardEnemyField4.ID, gameManager, gameManager.CardEnemyField4.WhoseCard);
-                        //fieldOn.ShowCardInfo(fieldOn.SelfCard, fieldOn.ID, gameManager, fieldOn.WhoseCard);
-                        //return;
+                        fieldOn.ShowCardInfo(fieldOn.SelfCard, fieldOn.ID, gameManager, fieldOn.WhoseCard);
+                        return;
                     }
                     else
                     {
